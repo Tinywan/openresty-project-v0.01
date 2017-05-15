224adb071dce9e88575645ca3a87a085c6123629
756684177 <756684177@qq.com>
2017-05-15 22:27:12 +0800

    添加Nginx 启动、停止脚本

---
+++ bin/start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# get Nginx Pid
+PID=$(ps -aef | grep nginx | grep -v grep | grep master |awk '{print $2}')
+echo $PID "\n"
+if [[ $? == 0 && -n $PID ]]
+then
+    echo -e "\033[35m [ Nginx running ] \033[0m"
+    #确认nginx配置文件的语法是否正确，否则nginx将不会加载新的配置文件
+    sudo /opt/openresty/nginx/sbin/nginx  -t -c /mnt/hgfs/Linux-Share/Lua/lua_project_v0.01/conf/nginx.conf
+    #sudo /opt/openresty/nginx/sbin/nginx  -s reload
+    sudo kill -HUP $PID
+    echo -e "\033[33m [ Nginx has reload ] \033[0m"
+else
+    echo -e "\033[31m [ Stop OK ] \033[0m"
+    sudo /opt/openresty/nginx/sbin/nginx  -t -c /mnt/hgfs/Linux-Share/Lua/lua_project_v0.01/conf/nginx.conf
+    sudo /opt/openresty/nginx/sbin/nginx  -c /mnt/hgfs/Linux-Share/Lua/lua_project_v0.01/conf/nginx.conf
+    echo -e "\033[32m [ Start OK ] \033[0m"
+fi
+exit 1
---
+++ bin/stop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# get Nginx Pid
+PID=$(ps -aef | grep nginx | grep -v grep | grep master |awk '{print $2}')
+sudo kill -QUIT ${PID}
+if [[ $? == 0 &&  -n $PID ]]
+then
+    echo -e "\033[33m [ Stop Success ] \033[0m"
+else
+    echo -e "\033[31m [ Stop Fail ] \033[0m"
+fi
+exit 1
