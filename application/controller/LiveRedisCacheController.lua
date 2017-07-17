c332c0ec77868e0cc5464080c871ebfa1288af82
Tinywan <756684177@qq.com>
2017-07-17 14:06:52 +0800

    mod log

--- application/controller/LiveRedisCacheController.lua
+++ application/controller/LiveRedisCacheController.lua
@@ -6,6 +6,7 @@
 * |  Date: 2017/5/19 23:25
 * |  Function: To change this template use File | Settings | File Templates.
 * |  TestInfo: port = 63700 auth = tinywan123456
+* |  log [n] 这样子的log日志是可以删除的（）
 * |------------------------------------------------------------------------
 --]]
 local template = require "resty.template"
@@ -21,15 +22,17 @@ local ERR = ngx.ERR
 local exit = ngx.exit
 local ngx_var = ngx.var
 local print = ngx.print
-local live_ngx_cache = ngx.shared.live_ngx_cache;
-local live_room = ngx.shared.live_room;
+local live_ngx_cache = ngx.shared.live_ngx_cache
+local live_room = ngx.shared.live_room
+-- 非error 日志开关 1:开启，0:关闭
+local log_switch = 1
 
 local redis_host = "121.41.88.209"
 local redis_port = 63789
 local redis_auth = "tinywanredisamaistream123456789"
 local redis_timeout = 1000
 
------------------ set ngx.cache
+-- set ngx.cache
 local function set_cache(key, value, exptime)
     if not exptime then
         exptime = 0
@@ -92,7 +95,10 @@ local function read_redis(_host, _port, _auth, keys)
         resp = nil
     end
     close_redis(red)
-    log(ERR, " [read_redis] content from redis.cache  id = " .. keys[1]) -- tag data origin
+    -- [1] del log
+    if log_switch == 1 then
+        log(ERR, "[2] [read_redis] content from redis.cache  id = " .. keys[1]) -- tag data origin
+    end
     return resp
 end
 
@@ -143,7 +149,9 @@ local function read_cache(key)
     -- step 1
     local val, err = live_ngx_cache:get(key)
     if val then
-        log(ERR, " [get_cache]  content from ngx.cache id = " .. key) -- tag data origin
+        if log_switch == 1 then
+            log(ERR, " [1] [read_ngx_cache]  content from ngx.cache id = " .. key) -- tag data origin
+        end
         return val
     end
 
@@ -186,7 +194,7 @@ local function read_cache(key)
         end
         -- FIXME: we should handle the backend miss more carefully
         -- here, like inserting a stub value into the cache.
-        log(ERR, "redis no value found : ", err)
+        log(ERR, "[4] ngx.cache find redis cache no value found : ", err)
         return ngx_resp
     end
 
@@ -238,15 +246,17 @@ local function read_http(id)
 
     -- backend not data 判断api返回的状态码
     local status_code = helper.cjson_decode(resp.body)['code']
-    if tonumber(status_code) ~= 404 then
+    if tonumber(status_code) == 200 then
         -- 正常数据缓存到 Redis 数据缓存
         local live_info_key = "LIVE_TABLE:" .. id
-        local live_value = helper.cjson_decode(resp.body) -- 解析的Lua自己的然后存储到Redis 数据库中去
+        local live_value = helper.cjson_decode(resp.body)-- 解析的Lua自己的然后存储到Redis 数据库中去(这里最好使用lua的json格式去写入)
         local live_live_str = write_redis(redis_host, redis_port, redis_auth, { live_info_key }, helper.cjson_encode(live_value))
         if not live_live_str then
             log(ERR, "redis set info error: ")
         end
-        log(ERR, " [read_http] content from backend API id : " .. id) -- tag data origin
+        if log_switch == 1 then
+            log(ERR, "[3] [read_http] content from backend API id : " .. id) -- tag data origin
+        end
         return helper.cjson_encode(live_value)
     else
         -- 后端没有数据直接返回 nil
@@ -260,14 +270,12 @@ end
 local id = ngx_var.id
 local live_info_key = "LIVE_TABLE:" .. id
 
--- chat room num
-local succ, err, forcible = live_room:set("chat", id)
 -------- get ngx.cache content 这里遇到的坑就是，注意这里传递的自定义的key
-local content = read_cache(id)
+local content = read_cache(live_info_key)
 
 --if redis not request backend API and udpate redis cache
 if not content then
-    log(ERR, "redis not found content, back to backend API , id : ", id)
+    log(ERR, "[5] redis not found content, back to backend API , id : ", id)
     content = read_http(id)
 end
 
